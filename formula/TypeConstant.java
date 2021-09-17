88dea37749e930c6d45a9a3ab6cbaf804a206728
Seweryn <87577593+xsk07@users.noreply.github.com>
2021-09-17 20:06:29 +0200

    Delete formula directory

--- formula/TypeConstant.java
+++
@@ -1,9 +0,0 @@
-package formula;
-
-/** The TypeConstant class represents the set of the types of the formulas,
- * which can be operator or atomic. */
-public enum TypeConstant {
-
-    OPERATOR, ATOM
-
-}
