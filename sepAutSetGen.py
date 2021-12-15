df15b4778cf7d93cdd35f5f65910e35a36d98d74
Seweryn <87577593+xsk07@users.noreply.github.com>
2021-12-15 16:12:21 +0100

    bug fixed

--- sepAutSetGen.py
+++ sepAutSetGen.py
@@ -4,11 +4,7 @@ import json
 import shutil
 import pydot
 from ltlf2dfa.parser.ltlf import LTLfParser
-
-
-if __name__ == "__main__":
-    main()
-
+from ltlf2dfa.parser.pltlf import PLTLfParser
 
 def main():
     ''' create a directory called automata
@@ -18,12 +14,12 @@ def main():
         shutil.rmtree('automata')
     os.mkdir('automata')
 
-    ''' read the json file containing the matrix representation 
+    ''' read the json file containing the matrix representation
     of the SNF of the formula and for each row of the matrix create a directory  '''
 
-    with open('matrix.json', encoding='utf-8-sig') as f: 
+    with open('matrix.json') as f:
         matrix = json.load(f)
-        
+
     for i in range(len(matrix)):
         os.mkdir('automata/{!s}'.format(i+1))
         save_dfa(matrix[i][0], i+1, 'past')
@@ -32,14 +28,17 @@ def main():
 
 
 def save_dfa(phi, row, time):
-    parser = LTLfParser()
+    if(time == 'past'): parser = PLTLfParser()
+    else: parser = LTLfParser()
     formula = parser(phi)
     dfa = formula.to_dfa()
     graphs = pydot.graph_from_dot_data(dfa)
     graph = graphs[0]
-    graph.write_png('automata/{!s}/{}/.png'.format(row, time))
+    graph.write_png('automata/{!s}/{}.png'.format(row, time))
 
 
+if __name__ == "__main__":
+    main()
 
 
 
